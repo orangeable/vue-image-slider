84651429f7b3334e190222a57f333b97912a0799
nocoded <josh@nocoded.com>
2024-05-21 07:49:11 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Image Slider with Vue
+
+## Example screenshot:
+
+![Dump](https://raw.githubusercontent.com/orangeable/vue-image-slider/master/example.webp)
+
+## How to run:
+
+Clone the repository and run with the following command:
+```# npm run serve```
+
+## Tutorial link:
+
+Full article at:  [Simple Image Slider with Vue](https://orangeable.com/vue/image-slider)
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+		<ImageSlider />
+	</div>
+</template>
+
+<script>
+	import ImageSlider from "./components/ImageSlider.vue";
+	
+	export default {
+		name: "App",
+		components: {
+			ImageSlider
+		}
+	};
+</script>
+
+<style>
+	div#app {
+		font-family: Arial, sans-serif;
+		color: #2c3e50;
+		text-align: center;
+		margin-top: 60px;
+	}
+</style>
